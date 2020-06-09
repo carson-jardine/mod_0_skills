e235dd17fb2e2c5a5fc9f0d8de309aa195c96d89
Carson Jardine <carsonjardine@gmail.com>
2020-06-09 16:10:31 -0600

    Add test file for variables

---
+++ test_variables.rb
@@ -0,0 +1,22 @@
+food = "Mashed Potatoes"
+p food
+is_delicious = true
+food = "Pad Thai"
+num2 = 4.0
+num7 = 42.0
+
+quotient = "#{num7 / num2}" #not sure if the syntax of this is correct but it was the only way I could make it run
+
+songs = ["Yesterday", "Reason", "Rain on Me", "Hello"]
+
+p food
+
+p songs
+
+songs.shift
+
+p food.length
+
+p quotient
+
+print songs
